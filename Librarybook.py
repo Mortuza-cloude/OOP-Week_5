3312594ef1f73ac7165fda506dae417fa0d569c7
Mortuza-cloude <mortuza.net1@gmail.com>
2026-09-15 23:53:16 +0900

    borrow Book

--- Librarybook.py
+++ Librarybook.py
@@ -3,3 +3,17 @@ class Book:
         self.title = title
         self.author = author
         self.is_borrowed = False
+
+    def borrow (self):
+        if self.is_borrowed:
+            print(f" {self.title} is already borrwed")
+            return
+        self.is_borrwed = True
+        print (f" {self.title} borrwed successfully")
+
+
+
+
+        
+
+
