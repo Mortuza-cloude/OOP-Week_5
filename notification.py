e16f3cf033eec1ffec42f76e38fa7ff32947af9a
Mortuza-cloude <mortuza.net1@gmail.com>
2026-09-22 19:05:26 +0900

    Push Notification

--- notification.py
+++ notification.py
@@ -9,3 +9,13 @@ class SMSNotification:
     def __init__(self, recipient, message):
         self.reipient = recipient
         self.message = message
+def send (self):
+    print(f" [SMS] To {self.recipient}: {self.message}")
+
+class PushNotification:
+    def __init__(self,recipient, message):
+        self.reipient = recipient
+        self.message = message  
+
+    def send(self):
+        
