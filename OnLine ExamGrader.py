6e4b1165d02547fb156b5cd092d34fa63c332da5
Mortuza-cloude <mortuza.net1@gmail.com>
2026-09-22 20:23:54 +0900

    MCQquistion

--- OnLine ExamGrader.py
+++ OnLine ExamGrader.py
@@ -4,5 +4,13 @@ class Question (ABC):
     def __init__ (self,text,correct_answere):
         self.text = text
         self.correct_answere =  correct_answere
-        
+
+    @abstractmethod
+    def check_answer(self, student_answere):
+        pass
+
+class MCQQuestion(Question):
+    def check_answer(self, student_answere):
+        return student_answere.strip().lower() == self.correct_answere.strip().lower()
+    
 
